0e8920e4b77ae3412ae8ba17419b63f3cfc7bfb1
ANJANA BABU <97722706+Anju2003@users.noreply.github.com>
2025-02-08 17:26:23 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,32 @@
+export const data = [
+    {
+      question: "Does the child struggle to learn the alphabet or recognize letters?",
+      option1: "YES",
+      option2: "NO",
+      ans: 1,
+    },
+    {
+      question: "Is it difficult to pronounce unfamiliar words?",
+      option1: "YES",
+      option2: "NO",
+      ans: 1,
+    },
+    {
+      question: "Does the child have difficulty rhyming words (e.g., cat, hat, bat)",
+      option1: "YES",
+      option2: "NO",
+      ans: 1,
+    },
+    {
+      question: " Does the child frequently guess words instead of reading them?",
+      option1: "YES",
+      option2: "NO",
+      ans: 1,
+    },
+    {
+      question: "Does the child have trouble sounding out words when reading?",
+      option1: "YES",
+      option2: "NO",
+      ans: 1,
+    },
+  ];
